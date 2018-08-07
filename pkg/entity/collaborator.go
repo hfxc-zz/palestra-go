116b3e2922b87a91ec4494fe33c2feda25ad5bb4
Heitor Fontes Xavier Correia <hfxc@cin.ufpe.br>
2018-08-07 09:41:54 -0300

    Added Collaborator related entities

---
+++ pkg/entity/collaborator.go
@@ -0,0 +1,30 @@
+package entity
+
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+// CollabLoginData A temporary structure to create a simple login
+type CollabLoginData struct {
+	Username string        `json:"username"`
+	Pwd      string        `json:"pwd"`
+	CollabID bson.ObjectId `json:"id"`
+}
+
+// Collaborator User data
+type Collaborator struct {
+	ID        bson.ObjectId `json:"id"`
+	Name      string        `json:"name"`
+	Email     string        `json:"email"`
+	Social    CollabSocial  `json:"social"`
+	CreatedAt time.Time     `json:"createdAt"`
+}
+
+// CollabSocial An embedded object inside Collaborator
+type CollabSocial struct {
+	Homepage string `json:"homepage"`
+	Github   string `json:"github"`
+	Twitter  string `json:"twitter"`
+}
